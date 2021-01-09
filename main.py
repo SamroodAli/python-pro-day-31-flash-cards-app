3ee78195e9b7386b76812c4903f11a874cfa81d7
SamroodAli <samroodali@live.com>
2021-01-09 15:20:37 +0530

    Add:buttons linked to on right and wrong events

--- main.py
+++ main.py
@@ -6,27 +6,19 @@ FONT = ("Courier", 20, "italic")
 WORD_FONT = ("Courier", 30, "bold")
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Flash Card App")
 window.minsize(height=750, width=900)
 window.config(bg=BACKGROUND_COLOR)
 
-# Wrong Button
-WRONG_IMAGE = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=WRONG_IMAGE, borderwidth=0, highlightthickness=0, bd= 0)
-wrong_button.grid(column=0, row=1)
-
-# Right Button
-RIGHT_IMAGE = PhotoImage(file="images/right.png")
-right_button = Button(image=RIGHT_IMAGE, borderwidth=0, highlightthickness=0, bd= 0)
-right_button.grid(column=3, row=1)
-
 # ---------------------------- NEW CARD ------------------------------- #
-# Canvas widget
+
 FRONT_IMAGE = PhotoImage(file="images/card_front.png")
 BACK_IMAGE = PhotoImage(file="images/card_back.png")
 
 
+# Canvas widget
 def card_side(side, language, word):
     card_canvas = Canvas(height=600, width=900, highlightthickness=0, bg=BACKGROUND_COLOR)
     card_canvas.create_image(450, 300, image=side)
@@ -41,6 +33,26 @@ def new_card():
     card_side(FRONT_IMAGE, "French", "Chambre")
     window.after(2000, card_side, BACK_IMAGE, "English", "Bedroom")
 
+# ---------------------------- RIGHT AND WRONG EVENTS and BUTTONS ------------------------------- #
+
+
+def on_right():
+    new_card()
+
+
+def on_wrong():
+    new_card()
+
+
+# Wrong Button
+WRONG_IMAGE = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=WRONG_IMAGE, borderwidth=0, highlightthickness=0, bd=0, command=on_wrong)
+wrong_button.grid(column=0, row=1)
+
+# Right Button
+RIGHT_IMAGE = PhotoImage(file="images/right.png")
+right_button = Button(image=RIGHT_IMAGE, borderwidth=0, highlightthickness=0, bd=0, command=on_right)
+right_button.grid(column=3, row=1)
 
 # ---------------------------- APP LAUNCH AND LOOP ------------------------------- #
 new_card()
